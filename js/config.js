93301fb4360a586ea0193f8fc80aa49e24b93d36
DevWithShakil <m.shakil720@gmail.com>
2025-09-22 19:05:51 +0600

    config minor updated

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = {
     // In date range
-    // startDate: '2025-09-22T00:00:00',
-    // endDate: '2025-09-23T13:25:00',
+    startDate: '2025-09-22T00:00:00',
+    endDate: '2025-09-25T13:25:00',
 
     // Out of date range (uncomment to test)
-    startDate: '2025-09-22T00:00:00',
-    endDate: '2025-09-22T13:25:00',
+    // startDate: '2025-09-22T00:00:00',
+    // endDate: '2025-09-22T13:25:00',
 
     quizDurationSec: 20 * 60,
     maxAttemptsInRange: 3,
